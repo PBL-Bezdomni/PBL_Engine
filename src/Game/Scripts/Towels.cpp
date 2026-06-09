8ba8b037155e5c15dac2c7e4aa5637fc52c4a762
Gortyn <gortyn@afterburn.games>
2026-06-09 23:21:27 +0200

    Fix tutorial arrow on towels and massage

--- src/Game/Scripts/Towels.cpp
+++ src/Game/Scripts/Towels.cpp
@@ -1,7 +1,9 @@
 #include "Towels.h"
+#include "TutorialArrow.h"
 
 void Towels::Awake()
 {
 	m_ObjectNeed = AnimalNeeds::Towels;
 	AOnsenObject::Awake();
+	m_TutorialArrow->SetStartPosition(glm::vec3(0.f, 10.f, 0.f));
 }
